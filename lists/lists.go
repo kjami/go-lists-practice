9e1e858f51943224f65a19654bcef6909a8d68e7
Kishor Jami <kishorjami@MacBook-Air.local>
2024-01-15 10:31:41 -0500

    practice: slices and their behaviour

--- lists/lists.go
+++ lists/lists.go
@@ -15,4 +15,27 @@ func ExecuteLists() {
 	fmt.Println(strSubArr2)
 	lastStrArr := strSubArr2[1:3]
 	fmt.Println(lastStrArr)
+
+	// To understand the behaviour of len and cap on arrays and slices
+	// Case 1: slicing from left
+	var intArr [5]int = [5]int{1, 2, 3, 4, 5}
+	firstSlicedArr := intArr[1:] //2, 3, 4, 5
+	fmt.Println(len(firstSlicedArr), cap(firstSlicedArr), firstSlicedArr)
+	secondSlicedArr := firstSlicedArr[1:2] // 3
+	fmt.Println(len(secondSlicedArr), cap(secondSlicedArr), secondSlicedArr)
+	// Perform slice on the same slice but provide full capacity on the right (and empty value on the left)
+	secondSlicedArr = secondSlicedArr[:3] // 3, 4, 5
+	fmt.Println(len(secondSlicedArr), cap(secondSlicedArr), secondSlicedArr)
+
+	// Case 2: slicing to the right
+	var anotherStrArr [5]string = [5]string{"one", "two", "three", "four", "five"}
+	firstSlicedStrArr := anotherStrArr[:3] //"one", "two", "three"
+	fmt.Println(len(firstSlicedStrArr), cap(firstSlicedStrArr), firstSlicedStrArr)
+	secondSlicedStrArr := firstSlicedStrArr[1:2] // "two"
+	fmt.Println(len(secondSlicedStrArr), cap(secondSlicedStrArr), secondSlicedStrArr)
+	// Perform slice on the same slice but provide full capacity on the right (and empty value on the left)
+	secondSlicedStrArr = secondSlicedStrArr[:4] // "two", "three", "four", "five"
+	fmt.Println(len(secondSlicedStrArr), cap(secondSlicedStrArr), secondSlicedStrArr)
+	firstSlicedStrArr = firstSlicedStrArr[:5] // "one", "two", "three", "four", "five"
+	fmt.Println(len(firstSlicedStrArr), cap(firstSlicedStrArr), firstSlicedStrArr)
 }
